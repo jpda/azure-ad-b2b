88bcb2501f367173df18d1ad617a484de3c8d0ea
jpda <john.dandison@live.com>
2017-12-02 14:55:03 -0500

    updated readme

---
+++ azure-ad-b2b-singletenant-web/Controllers/UserAdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace azure_ad_b2b_singletenant_web.Controllers
+{
+    public class UserAdminController : Controller
+{
+    // GET: /<controller>/
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+}
